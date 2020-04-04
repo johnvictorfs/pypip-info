56383d1354c8189d7feb89656543a62b1419d2be
John Victor <johnvictorfs@gmail.com>
2020-04-04 11:28:43 -0300

    First commit.

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+from .pypi import search_pypi
+
+DEBUG = True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app = Flask(__name__)
+
+
+@app.route("/search/<string:query>")
+def index(query):
+    """
+    Search PyPi API
+    """
+    return jsonify(search_pypi(query))
+
+
+if __name__ == '__main__':
+    app.run()
